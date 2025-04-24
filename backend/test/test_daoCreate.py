97f824f748c5352149bd55188b6db21b12d38120
Filippa Lissäng Öst <filippaliss@gmail.com>
2025-04-24 10:29:06 +0200

    started on the test_daoCreate.py

---
+++ backend/test/test_daoCreate.py
@@ -0,0 +1,97 @@
+import pytest
+from unittest.mock import MagicMock
+from src.util.dao import DAO
+
+class TestDAO:
+    def test_1(self):
+        mock_dao = MagicMock()
+        obj = DAO(mock_dao)
+        email = "user@example.com"
+
+        obj.dao.find.return_value = []
+
+        result = obj.get_user_by_email(email)
+        assert result == None
+
+    def test_2(self):
+        mock_dao = MagicMock()
+        obj = DAO(mock_dao)
+        email = "user@example.com"
+
+        obj.dao.find.return_value = ["user1"]
+
+        result = obj.get_user_by_email(email)
+        assert result == "user1"
+    
+    def test_3(self):
+        mock_dao = MagicMock()
+        obj = DAO(mock_dao)
+        email = "user@example.com"
+
+        obj.dao.find.return_value = ["user1", "user2"]
+
+        result = obj.get_user_by_email(email)
+        assert result == "user1"
+
+    def test_4(self):
+        mock_dao = MagicMock()
+        obj = DAO(mock_dao)
+        email = "user@example"
+
+        obj.dao.find.return_value = ["user1"]
+
+        with pytest.raises(ValueError):
+            obj.get_user_by_email(email)
+
+
+    def test_5(self):
+        mock_dao = MagicMock()
+        obj = DAO(mock_dao)
+        email = "user@com"
+
+        obj.dao.find.return_value = ["user1"]
+
+        with pytest.raises(ValueError):
+            obj.get_user_by_email(email)
+
+    def test_6(self):
+        mock_dao = MagicMock()
+        obj = DAO(mock_dao)
+        email = "user@examplecom"
+
+        obj.dao.find.return_value = ["user1"]
+
+        with pytest.raises(ValueError):
+            obj.get_user_by_email(email)
+
+    def test_7(self):
+        mock_dao = MagicMock()
+        obj = DAO(mock_dao)
+        email = "@example.com"
+
+        obj.dao.find.return_value = ["user1"]
+
+        with pytest.raises(ValueError):
+            obj.get_user_by_email(email)
+
+    def test_8(self):
+        mock_dao = MagicMock()
+        obj = DAO(mock_dao)
+        email = "user@example..com"
+
+        obj.dao.find.return_value = ["user1"]
+
+        with pytest.raises(ValueError):
+            obj.get_user_by_email(email)
+    
+    def test_9(self):
+        mock_dao = MagicMock()
+        obj = DAO(mock_dao)
+        email = "userexample.com"
+
+        obj.dao.find.return_value = ["user1"]
+
+        with pytest.raises(ValueError):
+            obj.get_user_by_email(email)
+    
+    
