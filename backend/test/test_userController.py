e4bb5dd5408f3e800f7baa4c6929035f91853258
ante237 <anton.helmersson00@gmail.com>
2025-04-24 10:13:20 +0200

    Removed blank line

--- backend/test/test_userController.py
+++ backend/test/test_userController.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
 from src.controllers.usercontroller import UserController
 
 class TestUserController:
